0938e9c6d49d5445cf2b40d5a721cae161b97eaf
Mike Tölle <m.toelle@web.de>
2020-01-19 00:33:29 +0100

    [TASK] add TCA Overrides

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,15 @@
+<?php
+defined('TYPO3_MODE') or die();
+
+/***************
+ * Plugin
+ */
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'Typo3graf.Stafflist',
+    'Personlist',
+    'Staff List'
+);
+
+$pluginSignature = str_replace('_', '', 'stafflist') . '_personlist';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:stafflist/Configuration/FlexForms/flexform_personlist.xml');
--- ext_tables.php
+++ ext_tables.php
@@ -5,18 +5,6 @@ call_user_func(
     function()
     {
 
-        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-            'Typo3graf.Stafflist',
-            'Personlist',
-            'Staff List'
-        );
-
-        $pluginSignature = str_replace('_', '', 'stafflist') . '_personlist';
-        $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:stafflist/Configuration/FlexForms/flexform_personlist.xml');
-
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('stafflist', 'Configuration/TypoScript', 'Staff List');
-
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_stafflist_domain_model_persons', 'EXT:stafflist/Resources/Private/Language/locallang_csh_tx_stafflist_domain_model_persons.xlf');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_stafflist_domain_model_persons');
 
