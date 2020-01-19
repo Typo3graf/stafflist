a1eb120515ec2851f4b0822c57fb738dde993e21
Mike Tölle <m.toelle@web.de>
2020-01-19 03:00:09 +0100

    [TASK] persistence problem with flexform fixed

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -9,7 +9,9 @@ defined('TYPO3_MODE') or die();
     'Personlist',
     'Staff List'
 );
-
 $pluginSignature = str_replace('_', '', 'stafflist') . '_personlist';
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'recursive,pages';
+
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:stafflist/Configuration/FlexForms/flexform_personlist.xml');
