8311e31f6d2bdc0e6b0c2c8b745b40f6baa383d3
Mike Tölle <m.toelle@web.de>
2020-01-19 21:24:29 +0100

    [TASK] add some flexform functions

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -11,7 +11,8 @@ defined('TYPO3_MODE') or die();
 );
 $pluginSignature = str_replace('_', '', 'stafflist') . '_personlist';
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'recursive,pages';
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'recursive,select_key,pages';
 
 $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:stafflist/Configuration/FlexForms/flexform_personlist.xml');
+
