3766348720f21662d3c132548a7ec36e0a6e435c
Mike Tölle <m.toelle@web.de>
2020-02-15 03:19:54 +0100

    [FIX] PHP-CS

--- Classes/Controller/TeamsController.php
+++ Classes/Controller/TeamsController.php
@@ -1,18 +1,22 @@
 <?php
+
 namespace Typo3graf\Stafflist\Controller;
 
-
-use TYPO3\CMS\Core\Utility\GeneralUtility;/***
+/***
  * This file is part of the "Staff List" Extension for TYPO3 CMS.
  * For the full copyright and license information, please read the
  * LICENSE.txt file that was distributed with this source code.
  *  (c) 2020 Development-Team <development@typo3graf.de>, Typo3graf media-agentur
  ***/
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 /**
  * TeamsController
  */
 class TeamsController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
+
     /**
      * Inject a teams repository to enable DI
      *
@@ -23,28 +27,23 @@ class TeamsController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
         $this->teamsRepository = $teamsRepository;
     }
 
-
     /**
      * action teamList
-     *
-     * @return void
      */
     public function teamsListAction()
     {
-        $teams = $this->teamsRepository->findDemanded(GeneralUtility::trimExplode(',',$this->settings['usergroup'], TRUE), $this->settings);
+        $teams = $this->teamsRepository->findDemanded(GeneralUtility::trimExplode(',', $this->settings['usergroup'], true), $this->settings);
 
         $this->view->assign('teams', $teams);
     }
 
     /**
      * action groupedByTeams
-     *
-     * @return void
      */
     public function groupedByTeamsAction()
     {
-        $demand ='';
-        $teams = $this->teamsRepository->findGroupedByTeams(GeneralUtility::trimExplode(',',$this->settings['usergroup'], TRUE), $this->settings);
+        $demand = '';
+        $teams = $this->teamsRepository->findGroupedByTeams(GeneralUtility::trimExplode(',', $this->settings['usergroup'], true), $this->settings);
 
         $this->view->assign('teams', $teams);
     }
